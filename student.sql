6800b7b12f8b3e669c0229f0485e0771c3f4b2eb
unknown <2485403898@qq.com>
2021-07-06 20:27:11 +0800

    update sql

---
+++ student.sql
@@ -0,0 +1,7 @@
+INSERT INTO `student`(`id`, `name`, `email`, `age`) VALUES (2, 'lisan', NULL, 22);
+INSERT INTO `student`(`id`, `name`, `email`, `age`) VALUES (10, '赵四', 'lili@qq.com', 28);
+INSERT INTO `student`(`id`, `name`, `email`, `age`) VALUES (11, '张三', NULL, 11);
+INSERT INTO `student`(`id`, `name`, `email`, `age`) VALUES (28, '六六', '2525@qq.com', 29);
+INSERT INTO `student`(`id`, `name`, `email`, `age`) VALUES (99, '测测', 'cece@qq.com', 38);
+INSERT INTO `student`(`id`, `name`, `email`, `age`) VALUES (100, '李白', '99@qq.com', 70);
+INSERT INTO `student`(`id`, `name`, `email`, `age`) VALUES (110, '李白', '99@qq.com', 70);
